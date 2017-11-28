a2de8c9bb2fec06acf7017158d72d6ce01422d4c
Alexander Huber <alih83@gmx.de>
2017-11-28 19:46:01 +0100

    added esint, refactor

--- controllers/camera.js
+++ controllers/camera.js
@@ -1,20 +1,21 @@
-const exec          = require('child_process').exec;
-const activateCmd   = 'motion -c /home/pi/motion.conf';
+const { exec }      = require("child_process").exec;
+
+const activateCmd   = "motion -c /home/pi/motion.conf";
 const timestampCmd  = "node -e 'console.log(Date.now())' > /home/pi/motion/stamp.txt";
-const deactivateCmd = 'killall motion';
-const removeCmd     = 'rm /home/pi/motion/*.jpg';
+const deactivateCmd = "killall motion";
+const removeCmd     = "rm /home/pi/motion/*.jpg";
 
 
 module.exports.activate = (req, res) => {
-  if(req.authenticated) {
-    exec(activateCmd, function(err, stdout, stderr) {
-      console.log('activated motion!');
+  if (req.authenticated) {
+    exec(activateCmd, () => {
+      console.log("activated motion!");
     });
-    exec(timestampCmd, function(err, stdout, stderr) {
-      console.log('made timestamp');
+    exec(timestampCmd, () => {
+      console.log("made timestamp");
     });
-    setTimeout(function(res) {
-      res.redirect('back');
+    setTimeout(() => {
+      res.redirect("back");
     }, 3000, res);
   } else {
     res.status(401).send();
@@ -22,11 +23,11 @@ module.exports.activate = (req, res) => {
 };
 
 module.exports.deactivate = (req, res) => {
-  exec(deactivateCmd, function(err, stdout, stderr) {
-    console.log('deactivated motion!');
+  exec(deactivateCmd, () => {
+    console.log("deactivated motion!");
   });
-  exec(removeCmd, function(err, stdout, stderr) {
-    console.log('removed jpg files!');
+  exec(removeCmd, () => {
+    console.log("removed jpg files!");
   });
-  res.redirect('back');
+  res.redirect("back");
 };
--- watcher.js
+++ watcher.js
@@ -1,37 +1,39 @@
-const sane          = require('sane');
-const motionWatcher = sane('/home/pi/motion', { glob: ['**/*.jpg'] });
-const snapWatcher   = sane('/home/pi/motion', { glob: ['**/lastsnap.jpg'] });
-const stamp         = '/home/pi/motion/stamp.txt';
+const sane          = require("sane");
+const Dropbox       = require("dropbox");
+const email         = require("emailjs");
+const fs            = require("fs");
+
+const motionWatcher = sane("/home/pi/motion", { glob: ["**/*.jpg"] });
+const snapWatcher   = sane("/home/pi/motion", { glob: ["**/lastsnap.jpg"] });
+const stamp         = "/home/pi/motion/stamp.txt";
 const snapPath      = "/home/pi/motion/lastsnap.jpg";
-const fs            = require('fs');
-const Dropbox       = require('dropbox');
-const email         = require('emailjs');
+
 const server        = email.server.connect({
-   user:     process.env.EMAIL,
-   password: process.env.EMAIL_PW,
-   host:     process.env.EMAIL_HOST,
-   port:     "25",
-   tls:      true
+  user: process.env.EMAIL,
+  password: process.env.EMAIL_PW,
+  host: process.env.EMAIL_HOST,
+  port: "25",
+  tls: true,
 });
 
 const emailImage = (fullPath, fileName) => {
   console.log("");
   console.log("******************************");
-  console.log("Sending E-Mail with image file: " + fileName);
-  fs.readFile(fullPath, function(err, cont) {
+  console.log(`Sending E-Mail with image file: ${fileName}`);
+  fs.readFile(fullPath, (err, cont) => {
     const message = {
-      text:    "intruder detected, image attached", 
-      from:    process.env.EMAIL,
-      to:      process.env.EMAIL,
+      text: "intruder detected, image attached",
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
       subject: "intruder alert",
-      attachment: 
+      attachment:
       [
-          {data: cont, alternative: false},
-          {path: fullPath, type: "image/jpeg", name: fileName}
-      ]
+        { data: cont, alternative: false },
+        { path: fullPath, type: "image/jpeg", name: fileName },
+      ],
     };
-    server.send(message, function(err, message) {
-      if(err) {
+    server.send(message, () => {
+      if (err) {
         console.log("error sending e-mail");
         console.log(err);
         console.log("******************************");
@@ -44,35 +46,35 @@ const emailImage = (fullPath, fileName) => {
 };
 
 const uploadImage = (fullPath, fileName) => {
-    const dbx = new Dropbox({ accessToken: process.env.DBX_TOKEN });
-    console.log("");
-    console.log("******************************");
-    console.log("Uploading image file: " + fileName);
-    fs.readFile(fullPath, function(err, cont) {
-      dbx.filesUpload({path: '/camera/' + fileName, contents: cont})
-        .then(function(response) {
-          console.log("file uploaded");
-          console.log("******************************");
-          console.log("");
-        })
-        .catch(function(error) {
-          console.log("error uploading file");
-          console.error(error);
-          console.log("******************************");
-          console.log("");
-        });
-    });
-    return false;
+  const dbx = new Dropbox({ accessToken: process.env.DBX_TOKEN });
+  console.log("");
+  console.log("******************************");
+  console.log(`Uploading image file: ${fileName}`);
+  fs.readFile(fullPath, (err, cont) => {
+    dbx.filesUpload({ path: `/camera/${fileName}`, contents: cont })
+      .then(() => {
+        console.log("file uploaded");
+        console.log("******************************");
+        console.log("");
+      })
+      .catch((error) => {
+        console.log("error uploading file");
+        console.error(error);
+        console.log("******************************");
+        console.log("");
+      });
+  });
+  return false;
 };
 
 module.exports.watch = () => {
-  motionWatcher.on('add', function (filepath, root, stat) {
-    fs.readFile(stamp, function(err, cont){
-      var timeStamp = new Date(parseInt(cont)).getTime();
-      if(Date.now() > timeStamp + 30000) {
-        console.log('file added: ', filepath);
-        const fullPath = "/home/pi/motion/" + filepath;
-        if(!filepath.includes("snapshot") && !filepath.includes("lastsnap")) {
+  motionWatcher.on("add", (filepath) => {
+    fs.readFile(stamp, (err, cont) => {
+      const timeStamp = new Date(parseInt(cont, 10)).getTime();
+      if (Date.now() > timeStamp + 30000) {
+        console.log("file added: ", filepath);
+        const fullPath = `/home/pi/motion/${filepath}`;
+        if (!filepath.includes("snapshot") && !filepath.includes("lastsnap")) {
           emailImage(fullPath, filepath);
         }
       }
@@ -80,26 +82,26 @@ module.exports.watch = () => {
   });
 
   const buildFileName = () => {
-      var dateObj      = new Date();
-      var isoDateArray = dateObj.toLocaleDateString().split("/");
-      var date         =
-        isoDateArray[1] + "-" + isoDateArray[0] + "-" + isoDateArray[2];
-      var time         = dateObj.toTimeString().split(" ")[0].replace(/:/g, "-");
-      return date + "--" + time + ".jpg";
+    const dateObj      = new Date();
+    const isoDateArray = dateObj.toLocaleDateString().split("/");
+    const date         =
+      `${isoDateArray[1]} - ${isoDateArray[0]} - ${isoDateArray[2]}`;
+    const time         = dateObj.toTimeString().split(" ")[0].replace(/:/g, "-");
+    return `${date} -- ${time}.jpg`;
   };
 
-  snapWatcher.on('change', function (filepath, root, stat) {
+  snapWatcher.on("change", (filepath) => {
     // wait to prevent file link errors
-    setTimeout(function() {
-      console.log('snapshot changed: ', filepath);
+    setTimeout(() => {
+      console.log("snapshot changed: ", filepath);
       uploadImage(snapPath, buildFileName());
     }, 1000);
   });
 
-  snapWatcher.on('add', function (filepath, root, stat) {
+  snapWatcher.on("add", (filepath) => {
     // wait to prevent file link errors
-    setTimeout(function() {
-      console.log('snapshot added: ', filepath);
+    setTimeout(() => {
+      console.log("snapshot added: ", filepath);
       uploadImage(snapPath, buildFileName());
     }, 1000);
   });
